c4b3f09dcc81e73f82300bad023b618607dc005e
Devang Jaiswal <devangjaiswal18@gmail.com>
2026-06-03 13:30:24 +0530

    Update move_zeoes.cpp

--- Week_1/Day_3/move_zeoes.cpp
+++ Week_1/Day_3/move_zeoes.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> temp(nums.size());
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                temp[j] = nums[i];
+                j++;
+            }
 
+ 
+        }
+        for (; j < nums.size(); j++) {
+            temp[j] = 0;
+        }
+        nums=temp;
+
+    }
+};
