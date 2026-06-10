dd34ee4c8ad428fc461a6fbe9a1cbecaf4980b12
Devang Jaiswal <devangjaiswal18@gmail.com>
2026-06-10 15:33:47 +0530

    Update valid_anagram.cpp

--- Week_2/Day_1/valid_anagram.cpp
+++ Week_2/Day_1/valid_anagram.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       
+        int length_s=s.length();
+        int lengtht=t.length();
+        if(length_s!=lengtht) return false;
 
+        unordered_map <char,int> counts;
+        unordered_map <char,int> countt;
+        for(int i=0;i<length_s;i++)
+        {
+            counts[s[i]]++;
+            countt[t[i]]++;
+        }
+        if(countt==counts)
+        return true;
+        else 
+        return false;
+
+
+        
+    }
+};
